02dde2e3e6714f4e23bb168b4103843c18d7d50f
MauriceUzi <159530482+MauriceUzi@users.noreply.github.com>
2024-02-09 11:02:52 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Playing MansorV","Watching Tickets","Playing GTA VI"];
 
 
 let currentIndex = 0;
